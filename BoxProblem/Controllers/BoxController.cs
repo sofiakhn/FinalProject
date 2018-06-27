2c8babf0935b3af962cdc0f42342b63618e99dfc
me <20hussan@elmbrookstudents.org>
2018-06-27 13:39:27 -0500

    Finished Index Sort

--- BoxProblem/Controllers/BoxController.cs
+++ BoxProblem/Controllers/BoxController.cs
@@ -15,9 +15,45 @@ namespace BoxProblem.Controllers
         {
             service = new BoxService(context);
         }
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder)
         {
-            return View(service.GetAllBoxes());
+            ViewBag.IdSortParm = String.IsNullOrEmpty(sortOrder) ? "Id_desc" : "";
+            ViewBag.InventoryCountSortParm = sortOrder == "InventoryCount" ? "InventoryCount_desc" : "InventoryCount";
+            ViewBag.WeightSortParm = sortOrder == "Weight" ? "Weight_desc" : "Weight";
+            ViewBag.VolumeSortParm = sortOrder == "Volume" ? "Volume_desc" : "Volume";
+            ViewBag.CostSortParm = sortOrder == "Cost" ? "Cost_desc" : "Cost";
+            var boxes = from b in service.GetAllBoxes() select b;
+            switch (sortOrder)
+            {
+                case "Id_desc":
+                    boxes = boxes.OrderByDescending(b => b.Id);
+                    break;
+                case "InventoryCount_desc":
+                    boxes = boxes.OrderByDescending(b => b.InventoryCount);
+                    break;
+                case "InventoryCount":
+                    boxes = boxes.OrderBy(b => b.InventoryCount);
+                    break;
+                case "Weight_desc":
+                    boxes = boxes.OrderByDescending(b => b.Weight);
+                    break;
+                case "Weight":
+                    boxes = boxes.OrderBy(b => b.Weight);
+                    break;
+                case "Volume_desc":
+                    boxes = boxes.OrderByDescending(b => b.Volume);
+                    break;
+                case "Volume":
+                    boxes = boxes.OrderBy(b => b.Volume);
+                    break;
+                case "Cost_desc":
+                    boxes = boxes.OrderByDescending(b => b.Cost);
+                    break;
+                case "Cost":
+                    boxes = boxes.OrderBy(b => b.Cost);
+                    break;
+            }
+            return View(boxes.ToList());
         }
     }
 }
