a82069123882f483df0e53509d3794787b267303
SamzFerg <SamzFerg@gmail.com>
2018-06-27 11:26:21 -0500

    Added Filter Methods in Repository

--- BoxProblem/Repositories/BoxRepository.cs
+++ BoxProblem/Repositories/BoxRepository.cs
@@ -13,5 +13,29 @@ namespace BoxProblem.Repositories
         {
             dbContext = context;
         }
+
+        public List<BoxInventory> FilterByWeight(int MinWeight, int MaxWeight)
+        {
+            List<BoxInventory> Boxes = dbContext.Boxes.Where(b => b.Weight <= MaxWeight && b.Weight >= MinWeight).ToList();
+            return Boxes;
+        }
+
+        public List<BoxInventory> FilterByVolume(int MinVolume, int MaxVolume)
+        {
+            List<BoxInventory> Boxes = dbContext.Boxes.Where(b => b.Volume <= MaxVolume && b.Volume >= MinVolume).ToList();
+            return Boxes;
+        }
+
+        public List<BoxInventory> FilterByCost(int MinCost, int MaxCost)
+        {
+            List<BoxInventory> Boxes = dbContext.Boxes.Where(b => b.Cost <= MaxCost && b.Cost >= MinCost).ToList();
+            return Boxes;
+        }
+
+        public List<BoxInventory> FilterByLiquidProof(bool isLiquidProof)
+        {
+            List<BoxInventory> Boxes = dbContext.Boxes.Where(b => b.CanHoldLiquid == isLiquidProof).ToList();
+            return Boxes;
+        }
     }
 }
